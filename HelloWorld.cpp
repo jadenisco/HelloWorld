5372d53afa557a7b018105baf87c0df033aeff94
John DeNisco <jadenisco17@gmail.com>
2022-04-18 07:54:12 -0400

    Initial Commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.cpp
+//
+//  Created by John DeNisco on 2/5/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# HelloWorld.cpp
+
+This repository is to be used to get debug and create a VSCode CPP environment.
